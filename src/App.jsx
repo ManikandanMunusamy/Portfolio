60e9ec566020b49cdfc36c6a1c297250dbf391bc
manikandan.munusamy <makdileep27@gmail.com>
2023-09-16 08:24:00 +0530

    Initial commit - setup tailwind config, compnents creation and styles.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Hero,
+  Contact,
+  Experience,
+  Feedbacks,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+const App = () => {
+  return (
+   <BrowserRouter>
+    <div className="relative z-0 bg-secondary">
+
+    </div>
+   </BrowserRouter>
+  );
+};
+
+export default App;
